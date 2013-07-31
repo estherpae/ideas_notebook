c999d71141848bf05d20766298a73c4716dee07f
Your name <you@email.com>
2013-07-31 18:35:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EstherIdeaNotebook::Application.config.secret_key_base = 'd2eee0dce91cd346b4aa0d368213b7641c43e56681b98a9bf3446b84ca24d9775f3715623fbd92c3b05056d3e4903b65fa3edaeed71a5349b2711590661e354e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EstherIdeaNotebook::Application.config.session_store :cookie_store, key: '_esther_idea_notebook_session'
